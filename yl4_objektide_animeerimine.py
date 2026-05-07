65ad2df7e9254abb785feb0a7a6021059c17b63c
Mikk-Gregor Rännel <mikk-gregor.rannel@vikk.ee>
2026-05-07 10:17:49 +0300

    yl4_objektide_animeerimine.py
    lisasin asju juurde

--- yl4_objektide_animeerimine.py
+++ yl4_objektide_animeerimine.py
@@ -10,51 +10,79 @@ ekraan = pygame.display.set_mode((laius, korgus))
 pygame.display.set_caption("car game - mikk-gregor")
 
 #pictures
-taust = pygame.image.load("X:\PyCharmMiscProject\yl4/bg_rally.jpg").convert()
-punane_auto = pygame.image.load("X:\PyCharmMiscProject\yl4/f1_red.png").convert_alpha()
-sinine_auto = pygame.image.load("X:\PyCharmMiscProject\yl4/f1_blue.png").convert_alpha()
+taust = pygame.image.load("X:/PyCharmMiscProject/yl4/bg_rally.jpg").convert()
+punane_auto = pygame.image.load("X:/PyCharmMiscProject/yl4/f1_red.png").convert_alpha()
+sinine_auto = pygame.image.load("X:/PyCharmMiscProject/yl4/f1_blue.png").convert_alpha()
 
-# Auto positsioonid
+#punase auto positsioon
 punane_rect = punane_auto.get_rect(center=(laius // 2, korgus - 50))
 
-#sinised autod ja mitu tykki
+#sinised autod
 sinised = []
 for i in range(3):
     x = random.randint(150, laius - 150)
     y = random.randint(-400, -50)
-    sinised.append(pygame.Rect(x, y, sinine_auto.get_width(), sinine_auto.get_height()))
+    sinised.append(pygame.Rect(x, y,
+                                sinine_auto.get_width(),
+                                sinine_auto.get_height()))
 
 kiirus = 5
+auto_kiirus = 7
 skoor = 0
 font = pygame.font.SysFont("Arial", 24)
 
 kell = pygame.time.Clock()
 
-#game tsykkel
+# ame loop
 while True:
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
 
+    #klahvide kontroll
+    nupud = pygame.key.get_pressed()
+
+    if nupud[pygame.K_LEFT]:
+        punane_rect.x -= auto_kiirus
+
+    if nupud[pygame.K_RIGHT]:
+        punane_rect.x += auto_kiirus
+
+    #et auto ei laheks screenist valja
+    if punane_rect.left < 120:
+        punane_rect.left = 120
+
+    if punane_rect.right > laius - 120:
+        punane_rect.right = laius - 120
+
     #background
     ekraan.blit(taust, (0, 0))
 
-    #siniste autode liikumine ylevalt alla
+    #siniste autode liikumine
     for rect in sinised:
         rect.y += kiirus
+
+        #kui auto jouab alla
         if rect.y > korgus:
             rect.y = random.randint(-200, -100)
             rect.x = random.randint(150, laius - 150)
             skoor += 1
 
+        #crash
+        if punane_rect.colliderect(rect):
+            print("game over, man.. game over!")
+            pygame.quit()
+            sys.exit()
+
         ekraan.blit(sinine_auto, rect)
 
-    #red auto (mis on staatiline)
+    #punane auto
     ekraan.blit(punane_auto, punane_rect)
 
     #scoreboard
-    tekst = font.render("Skoor: " + str(skoor), True, (255, 255, 255))
+    tekst = font.render("skoor: " + str(skoor), True, (255, 255, 255))
     ekraan.blit(tekst, (10, 10))
 
     pygame.display.flip()
